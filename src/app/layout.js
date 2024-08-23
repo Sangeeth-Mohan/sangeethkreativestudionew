2930a398507b34d8d8108b7a666961b557e22ba0
Sangeeth Mohan <sangeethkumar.mohan34@t-mobile.com>
2024-08-23 18:54:19 -0400

    Initial Push

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,11 @@
-import { Inter } from "next/font/google";
+import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
+import PageTransition from "@/components/PageTransition";
 
-const inter = Inter({ subsets: ["latin"] });
+const jetBrainsMono = JetBrains_Mono({ subsets: ["latin"],
+weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+variable: '--font-jetbrains-mono' });
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +15,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={jetBrainsMono.variable}>
+        <Header />
+        <PageTransition>
+          {children}
+        </PageTransition>
+        </body>
     </html>
   );
 }
